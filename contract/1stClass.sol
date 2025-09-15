c5e5ceaf621e4bd1d05c0a627d84d60f10970e94
slpngdmn <slpngdmn@gmail.com>
2025-09-16 02:11:34 +0600

    gpt

--- contract/1stClass.sol
+++ contract/1stClass.sol
@@ -2,13 +2,52 @@
 pragma solidity ^0.8.19;
 
 contract MyFirstContract{
-    string userName;
-    function setName(string memory finalName) public {
-        userName = finalName;
-    }
+    // string userName;
+    // function setName(string memory finalName) public {
+    //     userName = finalName;
+    // }
     
-    function get() public view returns(string memory){
-        return userName;
+    // function get() public view returns(string memory){
+    //     return userName;
+    // }
+
+//     string public name = "Emon";   // একটি variable
+
+//     function setName(string memory newname) public {
+//         name = newname;   // নতুন নাম সেট করবে
+//     }
+
+//     function getName() public view returns(string memory) {
+//     return name; //shudhu dekhar jonno view
+// }
+
+//     function calculate() public pure returns(uint) {
+//     uint x = 10;  //pure modify kore na abr read o kore na, just calculate kore.
+//     uint y = 20; //returns 
+//     return x + y;
+// }
+
+string public name;
+    uint public age;
+    uint public newAge; // Storage variable
+
+    function setName(string memory _name) public {
+        name = _name;
     }
+
+    function setAge(uint _age) public {
+        age = _age;
+    }
+
+    // Removed `view` because we're modifying `newAge`
+    function addAge(uint _addAge) public returns(uint) {
+        newAge = age + _addAge;
+        return newAge;
+    }
+
+    function getInfo() public view returns(string memory, uint, uint) {
+        return (name, age, newAge);
+    }
+
 }
 
